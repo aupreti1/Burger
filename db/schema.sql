4959cc55a5e499f3cc57f4e99018b2f3d6a447a3
aupreti1 <anaupreti@gmail.com>
2019-05-31 16:22:00 -0400

    worked on the schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(225) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY(id)
+);
