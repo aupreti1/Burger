6714bc772878a3d79325203d0fcf88ce16a0b0fd
aupreti1 <anaupreti@gmail.com>
2019-06-01 19:43:02 -0400

    worked on the controllers file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var burger = require("../models/burger.js")
+
+// Links to the index.handlebars
+router.get("/", function (req, res) {
+    burger.all(function (burger_data) {
+        console.log(burger_data);
+        res.render('./index', { burger_data });
+    })
+})
+
+// Update method
+router.put('/burgers/update', function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
+        res.redirect('/');
+    })
+})
+
+// Create
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect('/');
+    })
+})
+
+module.exports = router;
