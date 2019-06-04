fc8491344416705bd95ff7de049f38dfaffb2ed5
aupreti1 <anaupreti@gmail.com>
2019-06-04 15:48:30 -0400

    fixed css and added JAWS DB

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine('handlebars', exphbs({
 
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 //linking controller route
